c1d590175e60a0d6251446f486cf47375adfb85e
Justin <124119395+jstn-swm@users.noreply.github.com>
2023-12-02 00:14:05 -0800

    Update chess_board.py
    Minor Changes ;)

--- chess_board.py
+++ chess_board.py
@@ -2,10 +2,6 @@ import tkinter as tk
 import math
 from Const import*
 from files import*
-root =  tk.Tk()
-canvas = tk.Canvas(root, width=Width, height=Height)
-root.minsize(800, 800)
-canvas.pack()
 
 # Draw the squares of the chessboard
 for row in range(board_size):
@@ -20,7 +16,6 @@ for row in range(board_size):
             canvas.create_rectangle(x1, y1, x2, y2, outline=border_color, width=border_width, fill=tile_2_color)
 
 # Add images for chess pieces and scale them
-
 images = {
     'wp': tk.PhotoImage(file=pawn_white),
     'wr': tk.PhotoImage(file=rook_white),
@@ -52,17 +47,77 @@ board = [
     ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
     ['wr', 'wn', 'wb', 'wq', 'wk', 'wb', 'wn', 'wr'],
 ]
-points = [[0] * board_size] * board_size
+
+# Dictionary to store the current position of each piece
+piece_positions = {}
+
+# Function to get the chess piece at a given board position
+def get_piece(row, col):
+    return board[row][col];
+
+# Function to update the board position for a piece
+def update_position(row, col, piece):
+    board[row][col] = piece
+    piece_positions[piece] = (row, col)
+
+
+# Function to handle mouse click event
+def on_click(event):
+    global selected_piece, selected_piece_image, selected_piece_position, selected_piece_image_id
+    x, y = event.x, event.y
+    col = x // square_size
+    row = y // square_size
+    piece = get_piece(row, col)
+
+    if piece != '  ':
+        selected_piece = piece
+        selected_piece_image = images[selected_piece]
+        selected_piece_position = (row, col)
+        # canvas.delete(selected_piece_image_id)
+
+
+# Function to handle mouse drag event
+def on_drag(event):
+    if selected_piece is not None:
+        x, y = event.x, event.y
+        canvas.coords(selected_piece_image_id, x, y)
+
+
+# Function to handle mouse release event
+def on_release(event):
+    global selected_piece, selected_piece_image, selected_piece_position, selected_piece_image_id
+    if selected_piece is not None:
+        x, y = event.x, event.y
+        col = x // square_size
+        row = y // square_size
+        new_position = (row, col)
+        update_position(*selected_piece_position, '  ')  # Clear the old position
+        update_position(*new_position, selected_piece)  # Update the new position
+        canvas.coords(selected_piece_image_id, col * square_size, row * square_size)
+        selected_piece_image_id = None  # Delete the old piece image ID
+        selected_piece = None
+
+
+# Bind mouse events to functions
+canvas.bind("<Button-1>", on_click)
+canvas.bind("<B1-Motion>", on_drag)
+canvas.bind("<ButtonRelease-1>", on_release)
+
+# Place the pieces on the chessboard initially
 for row in range(board_size):
     for col in range(board_size):
         piece = board[row][col]
-        x = col * square_size
-        y = row * square_size
         if piece != '  ':
-            canvas.create_image(x + square_size // 2, y + square_size // 2, image=images[piece])
-        '''
-        points[col][row] = x + square_size // 2
-        points[col] = y + square_size // 2
-        '''
+            x = col * square_size
+            y = row * square_size
+            piece_image_id = canvas.create_image(x + square_size // 2, y + square_size // 2, image=images[piece])
+            piece_positions[piece] = (row, col)
+            images[piece] = piece_image_id
+
+# Variables to keep track of the selected piece and its image
+selected_piece = None
+selected_piece_image = None
+selected_piece_position = None
+selected_piece_image_id = None
 
 root.mainloop()
